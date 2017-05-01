1963c05b92deb5f409673edb5200a11ae94a7006
nsaia <nick@saia.me>
2017-05-01 15:09:19 -0400

    Fix flickity loading too fast

--- _js/carousel.js
+++ _js/carousel.js
@@ -3,6 +3,7 @@ import Flickity from "flickity";
 $(document).ready(function() {
 	var el = document.querySelector(".carousel");
 	var flick = new Flickity(el, {
-		contain: true
+		contain: true,
+		imagesLoaded: true
 	});
 });
